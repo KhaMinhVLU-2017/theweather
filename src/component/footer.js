ac3217c60d92a72c2a1b93de75bd56a7cbac7f43
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2019-03-03 19:03:02 +0700

    Update Style

--- src/component/footer.js
+++ src/component/footer.js
@@ -4,7 +4,7 @@ import { Col } from 'reactstrap'
 class Footer extends Component {
   render() {
     return (
-      <Col md={12} sm={12} xs={12}>
+      <Col md={12} sm={12} xs={12} style={{textShadow: '2px 2px 2px black'}}>
         <h5>Develop by JudasFate</h5>
       </Col>
     )
--- src/component/header.js
+++ src/component/header.js
@@ -5,11 +5,11 @@ class Header extends Component {
   render() {
     return (
       <Fragment >
-        <Col md={12} xs={12} sm={12}>
+        <Col md={12} xs={12} sm={12} style={{textShadow: '2px 2px 2px black'}}>
           <h1>{this.props.city}</h1>
           <p>{this.props.weather}</p>
         </Col>
-        <Col md={12} xs={12} sm={12}>
+        <Col md={12} xs={12} sm={12} style={{textShadow: '2px 2px 2px black'}}>
           <h1>{this.props.temp}&deg;C</h1>
         </Col>
       </Fragment>
